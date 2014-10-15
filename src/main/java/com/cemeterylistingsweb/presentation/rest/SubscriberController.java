8102ef753516511b8945c63d48a94b6ac81bfde0
Michael <Michael@192.168.144.52>
2014-10-15 18:03:41 +0200

    createsubscriber

---
+++ src/main/java/com/cemeterylistingsweb/presentation/rest/SubscriberController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cemeterylistingsweb.presentation.rest;
+
+import com.cemeterylistingsweb.services.AdminRegisterSubscriberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Michael
+ */
+@Controller
+@RequestMapping(value = "api/subscriber") 
+public class SubscriberController {
+    
+    @Autowired
+    AdminRegisterSubscriberService arss;
+    
+    
+    @RequestMapping(value = "newcemetery", method = RequestMethod.GET)
+    public String newCemetery(){
+        return "createSubscriber";
+    }
+    
+}
