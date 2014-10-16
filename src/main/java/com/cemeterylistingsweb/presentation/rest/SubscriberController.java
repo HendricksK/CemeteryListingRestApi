43eae371a890b80d412c6f8a584c2fa1e347bab9
Michael <Michael@192.168.144.52>
2014-10-16 11:58:20 +0200

    subscriber fixes and add create subscriber

--- src/main/java/com/cemeterylistingsweb/presentation/rest/SubscriberController.java
+++ src/main/java/com/cemeterylistingsweb/presentation/rest/SubscriberController.java
@@ -5,11 +5,14 @@
  */
 package com.cemeterylistingsweb.presentation.rest;
 
+import com.cemeterylistingsweb.domain.Subscriber;
 import com.cemeterylistingsweb.services.AdminRegisterSubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -27,6 +30,16 @@ public class SubscriberController {
     public String createSubscriber(){
         return "createSubscriber";
     }
+    
+    @RequestMapping(value = "create",method = RequestMethod.POST) // This the uri e.g http://localhost:8084/askweb/api/club/create
+    @ResponseBody //Converts output or response to JSON String
+    public String create(@RequestBody Subscriber subscriber) { // @RequestBody for converting incoming JSON call to Object
+        
+        arss.persist(subscriber);
+        System.out.println(" Create Subscriber Called ");
+        return "Subscriber created";
+    }
+    
     
     @RequestMapping(value = "subscriberDetails", method = RequestMethod.GET)
     public String SubscriberDetails(){
