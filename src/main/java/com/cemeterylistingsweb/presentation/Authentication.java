2b2421c467022158436ad11b11b6bbeb5d557b61
Kurvin Hendricks <kurvinhendricks@gmail.com>
2014-10-17 00:25:44 +0200

    check if username is available

--- src/main/java/com/cemeterylistingsweb/presentation/Authentication.java
+++ src/main/java/com/cemeterylistingsweb/presentation/Authentication.java
@@ -5,9 +5,15 @@
  */
 package com.cemeterylistingsweb.presentation;
 
+import com.cemeterylistingsweb.domain.Subscriber;
+import com.cemeterylistingsweb.services.ApproveSubscriberService;
+import com.cemeterylistingsweb.services.RetrieveAllSubscriberDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -17,6 +23,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "api/auth") 
 public class Authentication {
     
+    @Autowired
+    RetrieveAllSubscriberDetails getUser;
+    
+    @Autowired
+    ApproveSubscriberService checkUserName;
     
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String login(){
@@ -36,5 +47,16 @@ public class Authentication {
     @RequestMapping(value = "register", method = RequestMethod.GET)
     public String newSubscriber(){
         return "createSubscriber";
+    }
+    
+    @RequestMapping(value = "getUserDetails",method = RequestMethod.GET) //http://localhost:8084/askweb/api/club/1234
+    @ResponseBody
+    public Subscriber getUserDetails(@RequestParam String username) { 
+        return getUser.getSubscriberByUsername(username);
+    }
+    
+    @RequestMapping(value = "checkUserName",method = RequestMethod.GET) //http://localhost:8084/askweb/api/club/1234
+    public Boolean checkUserNameAvail(@RequestParam String username) { 
+        return checkUserName.avalaibleUsername(username);
     }
 }
